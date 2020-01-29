05d7b3c572422e46ef65ebfdfa3c5a7a9ebe572f
asaf-dad <asaf.tavor@gmail.com>
2020-01-29 23:00:26 +0200

    gui now loads list of income types from db

---
+++ gui/mainGui.py
@@ -0,0 +1,93 @@
+import sys
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import Qt, QLine
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import QLineEdit, QPushButton, QScrollArea, QLabel, QApplication, QWidget, QVBoxLayout, QHBoxLayout
+
+from consts import ColumnNames
+from expenseshandler import ExpensesHandler
+
+
+class MainGui(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(MainGui, self).__init__()
+        self.expenses_handler = ExpensesHandler()
+        uic.loadUi(r"C:\Users\Asaf\Desktop\expensesCalculator\gui\mainGui.ui", self)
+        self._init_widget_objects()
+
+    def _init_widget_objects(self):
+        self.central_widget = self.centralWidget()
+        self._init_total_money_text_edit()
+        self._init_update_button()
+        self._fill_expenses_container()
+
+
+    def _fill_expenses_container(self):
+        income_types = self.expenses_handler.get_all_income_types() * 50
+        self.expenses_scroll = self._find_widget(QScrollArea, 'scrollArea')
+        self.widget = QWidget()  # Widget that contains the collection of Vertical Box
+        self.vbox = QVBoxLayout()
+
+        for income in income_types:
+            horizontal_layout = QHBoxLayout()
+            label = QLabel(income[ColumnNames.INCOME_NAME])
+            open_button = QPushButton("Open")
+            horizontal_layout.addWidget(label)
+            horizontal_layout.addWidget(open_button)
+
+            self.vbox.addLayout(horizontal_layout)
+
+        self.widget.setLayout(self.vbox)
+
+        # Scroll Area Properties
+        self.expenses_scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.expenses_scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.expenses_scroll.setWidget(self.widget)
+
+    def _init_update_button(self):
+        self.update_button = self._find_widget(QPushButton, 'update_button')
+        self.update_button.clicked.connect(self._update_total_money)
+
+    def _update_total_money(self):
+        pass
+
+    def _init_total_money_text_edit(self):
+        self.total_money = self._find_widget(QLineEdit, 'total_money')
+
+    def _find_widget(self, type, name):
+        widget = self.findChild(type, name)
+        assert widget is not None, f"Failed to find {name} object!!"
+        return widget
+
+def _set_theme_darcula(app):
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+    app.setPalette(dark_palette)
+    app.setStyleSheet("QToolTip { "
+                           "color: #ffffff; "
+                           "background-color: #2a82da; "
+                           "border: 1px solid white; }")
+
+if __name__ == "__main__":
+
+    app = QApplication([])
+    # _set_theme_darcula(app)
+
+    main_gui = MainGui()
+    main_gui.show()
+
+    sys.exit(app.exec_())
